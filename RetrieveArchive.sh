ab818dbf0dccd24fe23cc265c7c1ebe8dbf017d5
Mica Funston <MicaFunston@MAC26.local>
2016-09-14 07:54:32 -0400

    Make script actually return success on success

--- RetrieveArchive.sh
+++ RetrieveArchive.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/expect
 exp_internal 1
-set timeout 9
+set timeout 15
 set username [lindex $argv 0]
 set password [lindex $argv 1]
 set hostname [lindex $argv 2]
-set passwordsuccess [lindex $argv 3] #expected password success string
-set separateretrieve [lindex $argv 4] # y = retrieve [enter] jobnumber n = retrieve jobnumber
+set pwsuccess [lindex $argv 3] 
+set separateretrieve [lindex $argv 4]
 set jobnumber [lindex $argv 5]
 
 #findfilesuccess = ""
@@ -39,8 +39,7 @@ send "$password\r"
 
 expect {
   timeout { send_user "\nLogin failed. Password incorrect.\n"; exit 1}
-  "$passwordsuccess"
-  #"[retrievals@quartz ~]$"
+  "$pwsuccess"  
 }
 
 send_user "\nPassword is correct\n"
